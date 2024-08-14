0a3d647d3f6a8fa3087f970687e2cc211bf8c3b4
Gustavo Lacerda <gurlacerda@gmail.com>
2024-08-14 13:03:38 +0100

    Create sideDishes Component

---
+++ src/components/sideDishesForm/SideDishesForm.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Button, Card } from 'antd';
+import Image from 'next/image';
+import { SideDishes } from '@/models/sideDishes';
+
+const { Meta } = Card;
+
+interface SideDishesFormProps {
+    handleAddIngredient: (newSideDish: SideDishes) => void;
+}
+
+// Dados dinâmicos para os acompanhamentos
+const sideDishesData = [
+    { id: '1', name: 'Small fries', image: '/IMG/batataPequena.jpg' },
+    { id: '2', name: 'Large fries', image: '/IMG/batataGrande.jpg' },
+    { id: '3', name: 'Home fries', image: '/IMG/batataCasa.jpg' },
+    { id: '4', name: 'Coca Cola', image: '/IMG/cocaCola.jpg' },
+    { id: '5', name: 'Orange Juice', image: '/IMG/orangeJuice.jpg' },
+    { id: '6', name: 'Water Bottle', image: '/IMG/waterBottle.jpg' },
+];
+
+export default function SideDishesForm({ handleAddIngredient }: SideDishesFormProps) {
+    return (
+        <div className="flex flex-wrap gap-4 p-4 bg-white">
+            {sideDishesData.map(dish => (
+                <Card
+                    key={dish.id}
+                    hoverable
+                    className="w-60 bg-white shadow-lg border border-gray-200 rounded-lg"
+                    cover={
+                        <div className="relative w-full h-40">
+                            <Image
+                            alt={dish.name}
+                            src={dish.image}
+                            fill
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            style={{ objectFit: 'cover' }} // Use style instead of objectFit prop
+                            className="rounded-t-lg"
+                        />
+                        </div>
+                    }
+                >
+                    <Meta
+                        title={dish.name}
+                        className="text-orange-600 font-semibold"
+                    />
+                    <Button
+                        type='primary'
+                        className="mt-4 bg-orange-500 border-orange-500 hover:bg-orange-600 hover:border-orange-600 text-white"
+                        onClick={() => handleAddIngredient(new SideDishes(dish.id, dish.name))}
+                    >
+                        Add
+                    </Button>
+                </Card>
+            ))}
+        </div>
+    );
+}
